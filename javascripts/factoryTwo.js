f0dc823f49b917f9599cfe7651f9dc275b558ee9
Joseph Kane <josephckane@gmail.com>
2016-06-02 14:40:16 -0500

    add delete method

--- javascripts/factoryTwo.js
+++ javascripts/factoryTwo.js
@@ -10,13 +10,13 @@ let fakeSentObject = {
 angular.module('movieHunter', [])
 	.factory('moviePath', ($http) => {
 
-		const FB_URL = 'https://movie-hunter.firebaseio.com';
+		const FB_URL = 'https://movie-hunter.firebaseio.com/movies';
 		let myMovies;
 
 		return {
 		// GET from firebase
 			getMovies () {
-					$http.get(`${FB_URL}/movies.json`)
+					$http.get(`${FB_URL}.json`)
 						.then(ret => {
 								myMovies = ret.data;
 								console.log("returned movies", myMovies);
@@ -26,13 +26,19 @@ angular.module('movieHunter', [])
 			},
 			// POST to firebase
 			postMovie (object, factory) {
-				$http.post(`${FB_URL}/movies.json`, object)
+				$http.post(`${FB_URL}.json`, object)
+					.then(factory.getMovies())
+			},
+			// DELETE from firebase
+			deleteMovie (id, factory) {
+				$http.delete(`${FB_URL}/${id}.json`)
 					.then(factory.getMovies())
 			}
 		}
 	})
 	.controller('fakeController', (moviePath) => (
-		moviePath.postMovie(fakeSentObject, moviePath)
+		// moviePath.postMovie(fakeSentObject, moviePath)
+		moviePath.deleteMovie('-KJH_uWxUKL5FXSLBR2d', moviePath)
 	)
 
 )
