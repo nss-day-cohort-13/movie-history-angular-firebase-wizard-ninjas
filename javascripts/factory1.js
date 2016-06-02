56cbfbc0f12f05383de5a66bf3b1db227fe0fa7d
Sulaiman.Allen <xalerons@gmail.com>
2016-06-02 16:25:23 -0500

    Added minor functionality to make sure a movie exists

--- javascripts/factory1.js
+++ javascripts/factory1.js
@@ -8,20 +8,23 @@ movieHunter
       $http.get(`http://www.omdbapi.com/?t=${arg}&r=json`)
         .then((response) => {
 
-          // actorArray = response.data.Actors.split(, );
+          if( response.data.Error != "Movie not found!") {
 
-          let film = {
-            title: response.data.Title,
-            year: response.data.Year,
-            actors: response.data.Actors.split(", "), // This needs to be an array
-            rating: Math.floor(response.data.imdbRating), // This needs to be an int
-            watched: false
-          };
-          // console.log("film", film);
+            let film = {
+              title: response.data.Title,
+              year: response.data.Year,
+              actors: response.data.Actors.split(", "), // This needs to be an array
+              rating: Math.floor(response.data.imdbRating), // This needs to be an int
+              watched: false
+            };
+
+            console.log("film = ", film);
+
+          } else {
+            console.log("Movie not found!")
+          }
 
           // controller2.
-
-
       });
     } 
   };
