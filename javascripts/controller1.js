7d87db62258b585b78deed4df8a9da8d1afcbdfa
Megan Ford <meg.c.ford@gmail.com>
2016-06-02 14:10:47 -0500

    search bar in DOM is connected to controller 1.

--- javascripts/controller1.js
+++ javascripts/controller1.js
@@ -1,22 +1,31 @@
 "use strict";
 
-angular.module("movieHunter", [])
-.controller("controller1", ['$scope', function($scope) {
+angular.module("movieHunter")
+  .controller("controller1", ($scope) => {
 
-  $scope.getSearchValue = () => {
-    // This is hardcoded for now
-    $scope.searchValue = "frozen";
-  };
+    // this is the blank value that will be sent on submit to the umdb search factory.
+    $scope.searchBarMovie = "";
+
+   //on click, the 'movie search bar' variable becomes the argument to a function called 'send search'.
+     $scope.sendSearch = function(movieToSearch) {
+      console.log("search bar movie", $scope.searchBarMovie, "movie to search", movieToSearch ); 
+     };
+
+     $scope.getSearchValue = () => {
+      // This is hardcoded for now
+      $scope.searchValue = "frozen";
+    };
+
+  });
+
+// angular.module('scopeExample', [])
+// .controller('MyController', ['$scope', function($scope) {
+//   $scope.username = 'World';
+
+//   $scope.sayHello = function() {
+//     $scope.greeting = 'Hello ' + $scope.username + '!';
+//   };
 
 
 
-}]);
-
-angular.module('scopeExample', [])
-.controller('MyController', ['$scope', function($scope) {
-  $scope.username = 'World';
-
-  $scope.sayHello = function() {
-    $scope.greeting = 'Hello ' + $scope.username + '!';
-  };
-}]);
+// }]);
