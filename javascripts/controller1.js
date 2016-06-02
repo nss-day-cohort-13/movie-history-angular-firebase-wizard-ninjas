8c532c380b20c8659fc045b159b6b3ee095c38a0
Megan Ford <meg.c.ford@gmail.com>
2016-06-02 14:33:53 -0500

    removed some stuff that conflicts with sule's work

--- javascripts/controller1.js
+++ javascripts/controller1.js
@@ -3,29 +3,6 @@
 angular.module("movieHunter")
   .controller("controller1", ($scope) => {
 
-    // this is the blank value that will be sent on submit to the umdb search factory.
     $scope.searchBarMovie = "";
-
-   //on click, the 'movie search bar' variable becomes the argument to a function called 'send search'.
-     $scope.sendSearch = function(movieToSearch) {
-      console.log("search bar movie", $scope.searchBarMovie, "movie to search", movieToSearch ); 
-     };
-
-     $scope.getSearchValue = () => {
-      // This is hardcoded for now
-      $scope.searchValue = "frozen";
-    };
-
+ 
   });
-
-// angular.module('scopeExample', [])
-// .controller('MyController', ['$scope', function($scope) {
-//   $scope.username = 'World';
-
-//   $scope.sayHello = function() {
-//     $scope.greeting = 'Hello ' + $scope.username + '!';
-//   };
-
-
-
-// }]);
