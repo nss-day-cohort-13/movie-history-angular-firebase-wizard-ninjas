cd78f2fce751d6c03d3ad01556c580a8d07fb29f
Megan Ford <meg.c.ford@gmail.com>
2016-06-03 10:40:49 -0500

    changed FB URL to parse correctly

--- javascripts/factoryTwo.js
+++ javascripts/factoryTwo.js
@@ -2,25 +2,26 @@
 Any controller that needs access to these methods
 MUST include this factory ('moviePath') in its function arguments
 */
+"use strict";
 
 movieHunter.factory("moviePath", (function($http) {
 
-		const FB_URL = "https://movie-hunter.firebaseio.com/movies";
-		let myMovies = {};
+  const FB_URL = "https://movie-hunter.firebaseio.com/";
+  let myMovies = {};
 
-		return {
-			// GET from firebase
-			getMoviesFromFirebase () {
-				return new Promise(function(resolve, reject) {
-					$http.get(`${FB_URL}.json`)
-						.then(ret => {
-							console.log("data", ret );
-								 myMovies = ret.data;
-								 resolve(myMovies);
-							}
-						);
-					});
-			},
+  return {
+    // GET from firebase
+    getMoviesFromFirebase () {
+      return new Promise(function(resolve, reject) {
+        $http.get(`${FB_URL}.json`)
+          .then(ret => {
+            console.log("data", ret );
+            myMovies = ret.data;
+            resolve(myMovies);
+          }
+        );
+      });
+    },
 			/*
 				POST to firebase; 'factory' argument allows
 				this method to call the getMoviesFromFirebase method
